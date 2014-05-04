7d984106a98a21312c41f525cbaea8e7fb87a2ce
deathcap <deathcap@gmx.co.uk>
2014-05-03 22:12:22 -0700

    Measure from clicked voxel instead of player position

--- measure.js
+++ measure.js
@@ -8,7 +8,7 @@ module.exports = function(game, opts) {
   return new MeasurePlugin(game, opts);
 };
 module.exports.pluginInfo = {
-  loadAfter: ['voxel-registry', 'voxel-recipes', 'voxel-console', 'voxel-player']
+  loadAfter: ['voxel-registry', 'voxel-recipes', 'voxel-console']
 };
 
 function MeasurePlugin(game, opts) {
@@ -17,9 +17,6 @@ function MeasurePlugin(game, opts) {
 
   this.console = game.plugins.get('voxel-console');
   if (!this.console) throw new Error('voxel-measure requires voxel-console plugin');
-
-  this.player = game.plugins.get('voxel-player');
-  if (!this.player) throw new Error('voxel-measure requires voxel-player plugin');
 
   this.startPos = undefined;
   this.endPos = undefined;
@@ -49,11 +46,16 @@ var strCoords = function(pos) {
 }
 
 MeasurePlugin.prototype.use = function(held, target) {
+  if (!target.voxel) {
+    this.console.log('Right-click a voxel to start measuring'); // TODO: use player position if not clicking a voxel?
+    return;
+  }
+
   if (!this.startPos) {
-    this.startPos = vec3.fromValues(this.player.position.x, this.player.position.y, this.player.position.z);
+    this.startPos = vec3.fromValues.apply(null, target.voxel);
     this.console.log('Starting position '+strCoords(this.startPos));
   } else {
-    var endPos = vec3.fromValues(this.player.position.x, this.player.position.y, this.player.position.z);
+    var endPos = vec3.fromValues.apply(null, target.voxel);
     var distance = vec3.distance(this.startPos, endPos);
 
     this.console.log(distance.toFixed(1) + ' from '+strCoords(this.startPos)+' - '+strCoords(endPos));
